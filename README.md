f603fec1204222870c561cda62419ad945df78f6
Phil | Doopee Doo <phil@doopeedoo.co.uk>
2024-07-19 16:46:26 +0100

    Add error handling logic(404 & 500) + template page: no design

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # M3Project
+
+
+
+Features
+Login
+
+Extends template
+Documentation : https://flask.palletsprojects.com/en/1.1.x/patterns/templateinheritance/
+
+Navbar
+Documentation
+https://getbootstrap.com/docs/4.0/components/navbar/
+
+Error page handling
+For errors: 404 and 500 only
--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-
+app.run(debug=True)
+# Authentication logic
 @app.route("/")
 def index():
     return render_template('index.html')
 
-if __name__ == "__main__":
-   app.run(debug=True)
+# Error Page Handling
+@app.errorhandler(404)
+def page_not_found_error(e):
+    print("page_not_found_error()")
+    return render_template('error-handling/404.html')
 
-
-# Authentication logic
-
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('error-handling/500.html')
 
 # User Interface logic
+@app.route("/dashboard")
+def dashboard():
+    return render_template('grantee/grantee-dashboard.html')
 
-    #Admin
-
-    #Grantee
-
-    #Granter
+if __name__ == "__main__":
+    app.run(debug=True)
